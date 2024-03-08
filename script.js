db659975b34896c8d125a31eb441b0a51c114bd3
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2024-03-08 20:59:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+// let test=document.getElementById("test");
+// test.innerText="a";
+let data=document.getElementById("fname");
+data.addEventListener("blur",()=>{
+let value=data.value;
+	let value2=value.toUpperCase();
+	// test.innerText=value2;
+data.value=value2;
+})
